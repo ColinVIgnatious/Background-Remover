d5e8fb29876762d8905402f9a7b6cf686883283d
Nandhu@112 <nandhuraj112@gmail.com>
2024-02-12 20:20:14 +0530

    add loading

--- src/test.jsx
+++ src/test.jsx
@@ -5,16 +5,19 @@ import { FaSyncAlt } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const FileUpload = () => {
   const [image, setImage] = useState(null);
-  const [isImage, setIsImage] = useState(true);
+  const [isImage, setIsImage] = useState(true)
   const [file1, setFile1] = useState('');
   const [loading, setLoading] = useState(false);
   const [isRemove, setIsRemove] = useState(true);
   const apiKey = "AsijaPngiPpr7MS1LvAPVzAP";
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(false)
+//   const apikey =import.meta.env.VITE_API_SECRET_KEY
 
   const handleFileChange = (e) => {
+    // console.log(apikey,"apikey")
     const file = e.target.files[0];
     if (file) {
       setFile1(file);
@@ -22,7 +25,7 @@ const FileUpload = () => {
       reader.onloadend = () => {
         setImage(reader.result);
       };
-      setIsImage(false);
+      setIsImage(false)
       reader.readAsDataURL(file);
     }
   };
@@ -44,7 +47,7 @@ const FileUpload = () => {
       const url = URL.createObjectURL(blob);
       setImage(url);
     } catch (error) {
-      toast.error('Unsupported image format');
+        toast.error('Unsupported image format');
       console.error('Error removing background:', error);
     }
 
@@ -59,60 +62,61 @@ const FileUpload = () => {
     a.click();
     document.body.removeChild(a);
   };
-
-  const reload = () => {
-    setImage(null);
-    setIsImage(true);
-    setIsRemove(true)
-  };
+  const reload=()=>{
+    setImage(null)
+    setIsImage(true)
+  }
 
   return (
     <div className="flex flex-col items-center justify-center space-y-4 p-[5rem]">
+     
       <div className="max-w-xs relative p-5">
         {image ? (
           <img src={image} alt="Uploaded" className="rounded-lg shadow-md p-4 bg-slate-100" />
         ) : (
-          <div className="border border-gray-300 rounded-lg text-center bg-white w-64 h-64 items-center justify-center pt-[rem]">
-            <div className='pl-[6.5rem] pt-[5rem]'>
-            <FaImage className="text-gray-500 mt-2 " size={44} />
+          <div className="border border-gray-300 rounded-lg bg-white w-64 h-64 items-center justify-center pt-[5rem] ">
+            {/* Placeholder */}
+            <div className='flex-row pl-[7rem]'>
+            <FaImage className="text-gray-500 mt-2" size={44} />
             </div>
-            
-            <span >Add your Image</span>
+         
+            <div className='flex-row pl-[5rem]'>
+            <span>Add your Image</span>
+            </div>
           </div>
+        
         )}
       </div>
 
-      {isImage && (
-        <label htmlFor="file-upload" className="relative cursor-pointer">
-          <span className="py-2 px-4 bg-[#37247d] text-white rounded-lg shadow-md inline-block">
-            Select an Image
-          </span>
-          <input
-            id="file-upload"
-            type="file"
-            accept="image/*"
-            onChange={handleFileChange}
-            className="hidden"
-          />
-        </label>
-      )}
-
+     {isImage? <label htmlFor="file-upload" className="relative cursor-pointer">
+        <span className="py-2 px-4 bg-[#37247d] text-white rounded-lg shadow-md inline-block">
+          Select an Image
+        </span>
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={handleFileChange}
+          className="hidden"
+        />
+      </label>:null}
       {image && (
         <div>
-          {loading ? (
-            <div className="flex justify-center mt-4">
-              <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-purple-500"></div>
+            
+          {isRemove ? (
+            <div className='flex'>
+            <button
+              onClick={handleRemoveBackground}
+              className="bg-red-500 text-white px-4 py-2 rounded"
+            >
+              Remove Background
+            </button>
+              <button onClick={reload} className="flex items-center ml-2 justify-center rounded-full bg-gray-200 hover:bg-gray-300 w-10 h-10">
+              <FaSyncAlt className="h-6 w-6 text-gray-600" />
+            </button>
             </div>
           ) : (
-            <>
-          {isRemove?  <div className="flex">
-              <button onClick={handleRemoveBackground} className="bg-red-500 text-white px-4 py-2 rounded">
-                Remove Background
-              </button>
-              <button onClick={reload} className="flex items-center ml-2 justify-center rounded-full bg-gray-200 hover:bg-gray-300 w-10 h-10">
-                <FaSyncAlt className="h-6 w-6 text-gray-600" />
-              </button>
-            </div>:  <div className='flex'>
+            <div className='flex'>
             <button
               onClick={handleDownload}
               className="bg-green-500 text-white px-4 py-2 rounded"
@@ -122,8 +126,7 @@ const FileUpload = () => {
             <button onClick={reload} className="flex items-center ml-2 justify-center rounded-full bg-gray-200 hover:bg-gray-300 w-10 h-10">
               <FaSyncAlt className="h-6 w-6 text-gray-600" />
             </button>
-            </div>}
-            </>
+            </div>
           )}
         </div>
       )}
